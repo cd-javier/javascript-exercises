47589699d08f200d76e3ad23a2faa38042c613f8
Javier Quiroga <contact@javierquiroga.com>
2024-11-17 22:24:57 +0000

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringReversed = [];
+    for (i = string.length; i >= 0; i--) {
+        stringReversed.push(string[i])
+    }
+    return stringReversed.join('')
 };
 
 // Do not edit below this line
