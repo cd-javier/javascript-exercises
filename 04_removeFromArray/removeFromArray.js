5902fdbc35e88beefd3b8f0128f31dcda097af29
Javier Quiroga <contact@javierquiroga.com>
2024-11-18 10:50:58 +0000

    Update exercise 04 with simpler solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,26 @@
-const removeFromArray = function (array, target1 = -1, target2 = -1, target3 = -1, target4 = -1)  {
-    const targetList = [target1, target2, target3, target4]
-    for (let i = 0; i < targetList.length; i++) {
-        const target = targetList[i]
-        let targetIndex = array.indexOf(target)
-        if (targetIndex < 0) { continue }
-        array.splice(targetIndex, 1);
-        targetIndex = array.indexOf(target)
-        if (targetIndex < 0) { continue }
-        i--
-    }
-    return array
-};
+// const removeFromArray = function (array, target1 = -1, target2 = -1, target3 = -1, target4 = -1)  {
+//     const targetList = [target1, target2, target3, target4]
+//     for (let i = 0; i < targetList.length; i++) {
+//         const target = targetList[i]
+//         let targetIndex = array.indexOf(target)
+//         if (targetIndex < 0) { continue }
+//         array.splice(targetIndex, 1);
+//         targetIndex = array.indexOf(target)
+//         if (targetIndex < 0) { continue }
+//         i--
+//     }
+//     return array
+// };
+
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
