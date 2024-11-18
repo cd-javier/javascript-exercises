f394750730be78506d55656df24eb473d0b3fca3
Javier Quiroga <contact@javierquiroga.com>
2024-11-18 11:11:05 +0000

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (numberOne, numberTwo) {
+  if (!Number.isInteger(numberOne) || !Number.isInteger(numberTwo)) return 'ERROR';
+  if (numberOne < 0 || numberTwo < 0) return "ERROR";
+  const starter = numberOne < numberTwo ? numberOne : numberTwo;
+  const finisher = numberOne > numberTwo ? numberOne : numberTwo;
+  let sum = 0;
+  for (let i = starter; i <= finisher; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
