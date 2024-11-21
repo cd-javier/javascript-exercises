49eb7413f94e497b51db61987154c90655fbfa66
Javier Quiroga <contact@javierquiroga.com>
2024-11-21 21:03:47 +0000

    Complete exercises 08 to 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const alpha = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const finalWord = word
+    .toLowerCase()
+    .split("")
+    .filter((letter) => alpha.includes(letter))
+    .join("");
+
+  const reversed = finalWord.split("").reverse().join('');
+
+  return finalWord === reversed;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (countArg) {
+  let count;
+  if (typeof countArg !== "number") {
+    count = parseInt(countArg);
+  } else {
+    count = countArg;
+  }
+  if (countArg < 0) return "OOPS";
 
+  let fibo = [0, 1];
+
+  for (i = 0; i <= count; i++) {
+    fibo.push(fibo[i] + fibo[i + 1]);
+  }
+
+  return fibo[count];
 };
 
 // Do not edit below this line
