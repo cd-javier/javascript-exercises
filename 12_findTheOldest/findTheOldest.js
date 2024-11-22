56bb7255928777b0c3be945193902128dc799a7c
Javier Quiroga <contact@javierquiroga.com>
2024-11-22 17:22:59 +0000

    Complete exercises 11 to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  const orderedArray = array.toSorted((firstPerson, nextPerson) => {
+    birthYearOne = firstPerson.yearOfBirth;
+    deathYearOne = firstPerson.yearOfDeath || new Date().getFullYear();
+    birthYearTwo = nextPerson.yearOfBirth;
+    deathYearTwo = nextPerson.yearOfDeath || new Date().getFullYear();
 
+    return deathYearTwo - birthYearTwo - (deathYearOne - birthYearOne);
+  });
+  return orderedArray[0];
 };
 
 // Do not edit below this line
